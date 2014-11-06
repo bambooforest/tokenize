12185fbc1909bdd6c8adc4953cc6da9ed1a0d6a6
Steven Moran <bambooforest@gmail.com>
2014-11-06 21:17:15 +0100

    getter of environments

--- get_environments.py
+++ get_environments.py
@@ -1,40 +1,23 @@
 """
-Get character environments given a list of words and a target character.
-
-Input should be UTF-8 plain text, one word per line.
-
+Get character environments given input and a target character. Input should be UTF-8 plain text, one word per line. Use Py 3.
 """
-
 import sys
-import codecs
-import operator
 import collections
 import unicodedata
 
-# input file
-infile = codecs.open(sys.argv[1], "r", "utf-8")
-
-# to remove a header uncomment
-header = infile.readline()
-
+infile = open(sys.argv[1], "r")
 target_char = sys.argv[2]
-
-# hash set of grapheme clusters
-environs = collections.defaultdict(int)
-
-for word in infile:
-    word = word.strip()
-    word = unicodedata.normalize("NFD", word)
-    word = "#"+word+"#"
-    indices = [i for i, ltr in enumerate(word) if ltr == target_char]
-
+results = []
+for line in infile:
+    line = line.strip()
+    line = unicodedata.normalize("NFD", line)
+    line = "#"+line+"#"
+    indices = [i for i, ltr in enumerate(line) if ltr == target_char]
     for i in indices:
-        print(word[1:-1], "\t", word[i-1]+"_"+word[i+1])
-        environment = word[i-1]+"_"+word[i+1]
-        environs[environment] += 1
+        environment = line[i-1]+"_"+line[i+1]
+        results.append(environment)
+environments = collections.Counter(results)
+for environ, count in environments.most_common():
+    print('%s\t%7d' % (environ, count))
 
-# evaluate every character in the document
-sorted_environs = sorted(environs.items(), key=operator.itemgetter(0))
 
-for environment, frequency in sorted_environs:
-    print(target_char, "\t", environment, "\t", frequency)
